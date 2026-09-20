c2831bd6c32985676489d9ead1ff8fcfe8f8e3e6
NelsonConde <nelsonenrriquect@ufps.edu.co>
2026-09-19 22:27:41 -0500

    fase 3: maquina de aplicacion con script de arranque

--- main.tf
+++ main.tf
@@ -22,3 +22,23 @@ resource "google_compute_subnetwork" "publica" {
   region        = var.region
   network       = google_compute_network.vpc.id
 }
+
+resource "google_compute_instance" "app" {
+  name         = "${var.prefijo}-app"
+  machine_type = var.tipo_maquina
+  zone         = var.zona
+  tags         = ["servidor-web"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.publica.id
+    access_config {}
+  }
+
+  metadata_startup_script = file("${path.module}/arranque.sh")
+}
